14f1ee62d15986def2adf5347e908f737513b18d
kpdebree <kpdebree@gmail.com>
2017-12-14 19:31:33 -0800

    Map Works!

--- public/js/index.js
+++ public/js/index.js
@@ -2,74 +2,103 @@ var comps;
 
 function getData() {
     $.get("/api/comps", function(data) {
-        console.log(data)
         comps = data;
+        initMap(data)
+        console.log(comps)
         
     })
-    placeMarkers();
+    // placeMarkers();
 };
 
+function initMap(locations) {
 
-function placeMarkers() {
-    // Place each marker on the map  
-    for( i = 0; i < comps.length; i++ ) {
-        console.log(comps[i])
-        var position = new google.maps.LatLng(comps[i].latitude, comps[i].longitude);
-        bounds.extend(position);
-        var marker = new google.maps.Marker({
-            position: position,
-            map: map,
-            title: comps[i].building_name
-        });
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(33.830834,-117.927206),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
 
-         google.maps.event.addListener(marker, 'click', (function(marker, i) {
-            return function() {
+    var infowindow = new google.maps.InfoWindow();
 
-                
+    var marker, i;
+    console.log(locations);
 
-                infoWindow.setContent('<div class="info_content"><h3>' + comps[i].building_name + '</h3></div>');
+    for (i = 0; i < locations.length; i++) { 
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+        map: map
+      });
 
-
-                infoWindow.open(map, marker);
-            }
-        })(marker, i));
-
-        // Center the map to fit all markers on the screen
-        map.fitBounds(bounds);
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i].building_name);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
     }
 }
 
 
+// function placeMarkers() {
+//     // Place each marker on the map  
+//     for( i = 0; i < comps.length; i++ ) {
+//         console.log(comps[i])
+//         var position = new google.maps.LatLng(comps[i].latitude, comps[i].longitude);
+//         bounds.extend(position);
+//         var marker = new google.maps.Marker({
+//             position: position,
+//             map: map,
+//             title: comps[i].building_name
+//         });
 
-function initMap(){
+//          google.maps.event.addListener(marker, 'click', (function(marker, i) {
+//             return function() {
 
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    var mapOptions = {
-        mapTypeId: 'terrain'
-    };
+                
+
+//                 infoWindow.setContent('<div class="info_content"><h3>' + comps[i].building_name + '</h3></div>');
+
+
+//                 infoWindow.open(map, marker);
+//             }
+//         })(marker, i));
+
+//         // Center the map to fit all markers on the screen
+//         map.fitBounds(bounds);
+//     }
+// }
+
+
+
+// function initMap(){
+
+//     var map;
+//     var bounds = new google.maps.LatLngBounds();
+//     var mapOptions = {
+//         mapTypeId: 'terrain'
+//     };
                     
-    // Display a map on the web page
-    map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
-    map.setTilt(50);
+//     // Display a map on the web page
+//     map = new google.maps.Map(document.getElementById("mapCanvas"), mapOptions);
+//     map.setTilt(50);
         
-    var infoWindow = new google.maps.InfoWindow(), marker, i;
+//     var infoWindow = new google.maps.InfoWindow(), marker, i;
 
-    getData();
+//     getData();
 
 
-     var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-        this.setZoom(8);
-        google.maps.event.removeListener(boundsListener);
-    });
-  // Load initialize function
-google.maps.event.addDomListener(window, 'load', initMap);  
-}
+//      var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+//         this.setZoom(8);
+//         google.maps.event.removeListener(boundsListener);
+//     });
+//   // Load initialize function
+// google.maps.event.addDomListener(window, 'load', initMap);  
+// }
 
 
 $(document).ready(function() { 
 
 
-    initMap();
+    getData();
   
 });
